b8f0d4cea4bca4658c544e2c825297a5864e0d2d
nguyenthanhtung-30092004 <tungnt2k4yyy@gmail.com>
2026-02-07 00:08:10 +0700

    mongodbBase

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,7 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+// nguyenthanhtung;
+// b8mRHjko9bnfYz10;
+const MONGODB_URI =
+  "mongodb+srv://nguyenthanhtung:b8mRHjko9bnfYz10@cluster0.tvqwfdx.mongodb.net/?appName=Cluster0";
